b7df05e317be35e5d036719df20013553647df83
Miguel Frade <miguel.frade@ipleiria.pt>
2019-07-02 17:16:28 +0100

    remove also jdk.sh file

--- oracle-java-installer.sh
+++ oracle-java-installer.sh
@@ -141,7 +141,6 @@ install_jdk() {
     mv /etc/profile.d/jdk.sh /etc/profile.d/jdk.`date +%F`.bak
     echo "export J2SDKDIR=/usr/lib/jvm/$VERSION" 2>&1 | tee /etc/profile.d/jdk.sh
     echo "export J2REDIR=/usr/lib/jvm/$VERSION/jre" 2>&1 | tee --append /etc/profile.d/jdk.sh
-    # echo "export PATH=\$PATH:/usr/lib/jvm/$VERSION/bin:/usr/lib/jvm/$VERSION/db/bin:/usr/lib/jvm/$VERSION/jre/bin" 2>&1 | tee --append /etc/profile.d/jdk.sh
     echo "export JAVA_HOME=/usr/lib/jvm/$VERSION" 2>&1 | tee --append /etc/profile.d/jdk.sh
     # echo "export DERBY_HOME=/usr/lib/jvm/$VERSION/db" 2>&1 | tee --append /etc/profile.d/jdk.sh
     
@@ -209,6 +208,7 @@ remove_jdk() {
     echo "[INFO ] Deleting $VERSION files ..."
     rm -r /usr/lib/jvm/$VERSION 2>&1
     rm /usr/lib/jvm/.$VERSION.jinfo 2>&1
+    rm /etc/profile.d/jdk.sh 2>&1
     
     echo "[INFO ] $VERSION is removed."
     
